d3387285dee417d1fbba12bc53e9ef82985262b1
JavierBurgara <135621096+JavierBurgara@users.noreply.github.com>
2023-07-31 21:03:53 -0700

    updated css for weather api

--- trailScript.js
+++ trailScript.js
@@ -6,6 +6,60 @@ var firstBeachID;
 var secondBeach;
 var thirdBeach;
 var usrCounty;
+
+
+let weather = {
+  apiKey: "8af713bebedab909ba40e0054699acdb",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°F";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " mph";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 async function logBeaches() {
   const response = await fetch(
@@ -26,6 +80,7 @@ async function searchTrails() {
   var usrCounty = select.options[index].value;
   console.log(usrCounty);
 
+  weather.fetchWeather(usrCounty);
   firstBeach = beaches.find((item) => item.COUNTY === usrCounty);
   firstBeachID = firstBeach.ID;
   console.log(firstBeachID);
